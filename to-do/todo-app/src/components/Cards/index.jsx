0dddcec824913924c2b14823ff15b731f85d623e
Amaia <amaia.semper@gmail.com>
2021-04-12 17:37:47 +0200

    Work on CardDetail: -Add title -Add input change card title -Add tasks and styles

--- to-do/todo-app/src/components/Cards/index.jsx
+++ to-do/todo-app/src/components/Cards/index.jsx
@@ -1,36 +1,19 @@
 import React from 'react';
 import './index.scss';
-
-const cards = [
-  { name: 'Work', color: 'blue' },
-  { name: 'Home', color: 'yellow' },
-  { name: 'Family', color: 'purple' },
-  { name: 'Shopping', color: 'red' }
-];
+import { Link } from 'react-router-dom';
+import cards from '../../constants/user';
 
 function Cards() {
   return (
     <section className="container__cards">
       {
         cards.map((card) => (
-          <a href="/" className={`card-item ${card.color}`}>
+          <Link to={`/card-details/${card.name}`} className={`card-item ${card.color}`}>
             <h2>{card.name}</h2>
-          </a>
+          </Link>
         ))
       }
-      {/* <a href="/" className="card-item blue">
-        <h2>{cards[0]}</h2>
-      </a>
-      <a href="/" className="card-item yellow">
-        <h2>Home</h2>
-      </a>
-      <a href="/" className="card-item purple">
-        <h2>Family</h2>
-      </a>
-      <a href="/" className="card-item red">
-        <h2>Shopping</h2>
-      </a> */}
-      <a href="/" className="card-item new">
+      <a href="/card-details" className="card-item new">
         <h2>New card +</h2>
       </a>
     </section>
